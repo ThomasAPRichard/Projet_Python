29f9200b069f19f14992086d7fa85e65a917e95f
ThomasRichard <thorichard@icloud.com>
2024-11-14 14:27:40 +0100

    Mise au propre avant de commencer

--- Partie1_linearmodel/Test_projet.py
+++ Partie1_linearmodel/Test_projet.py
@@ -1,16 +1,13 @@
+#FICHIER QUI NE DEVRA PAS ETRE DANS LE RENDU FINALE
+#CE FICHIER SERT A TESTER DES TRUCS ET DECOUVRIR UN PEU LE TRUC
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data_path = "C:/Users/tapri/OneDrive/Bureau/Master_1/Python/Projet_Python/Partie1_linearmodel/vehicles.csv"
 data = pd.read_csv(data_path)
-#print(data.head())
-
-# Afficher les informations de base sur le jeu de données
-#print(data.info())
-
-# Résumé statistique des variables numériques
-#print(data.describe())
 
 # Histogramme pour la variable 'Emission de CO2'
 plt.hist(data['CO2 emissions (g/km)'], bins=30, edgecolor='black')
